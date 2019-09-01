e9967e6269887357288dfc7b50279bc2a96b2221
Ukp123 <37942064+Ukp123@users.noreply.github.com>
2019-09-01 18:55:38 +0300

    First commit.

---
+++ src/main/java/io/github/ukp123/dkenhanced/DKEnhanced.java
@@ -0,0 +1,17 @@
+package io.github.ukp123.dkenhanced;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DKEnhanced extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
